4b6abb63923c671f04e266ede5ac3278399f7de0
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-11 01:52:54 +0800

    fix

--- app/[id]/head.js
+++ app/[id]/head.js
@@ -1,17 +1,21 @@
 export default async function Head({ params }) {
 
-    const res = fetch(`http://localhost:3000/api/hello?question=${params.id}`, {
+    const answer = await fetch(`http://localhost:3000/api/hello?question=${params.id}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ question: params.id })
     })
+    const data = await answer.json()
+    const res = data.completion
+
+    console.log("head" + res)
 
 
     return (
         <>
-            <title>{params.id.split("%20").join(" ")}</title>
+            <title>{res}</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
             <meta name="description" content={res} />
             <link rel="icon" href="/favicon.ico" />
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -9,14 +9,13 @@ const openai = new OpenAIApi(configuration);
 export async function getCompletion(question) {
   const res = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\". The maximum respond character is 249.\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: ${question}`,
+    prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\". The respond should be between 55 and 200 characters long, with a maximum of 300.\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: ${question}`,
     temperature: 0,
     max_tokens: 100,
     top_p: 1,
     frequency_penalty: 0.5,
     presence_penalty: 0,
   })
-  console.log(res.data.choices[0].text)
   return res.data.choices[0].text
 }
 
