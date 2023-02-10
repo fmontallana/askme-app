b5d5f8ea6caf4d42713891e6289190a637cd75b8
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-11 01:34:19 +0800

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Configuration, OpenAIApi } from 'openai'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function getCompletion(question) {
+  const res = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\". The maximum respond character is 249.\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: ${question}`,
+    temperature: 0,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
+  })
+  console.log(res.data.choices[0].text)
+  return res.data.choices[0].text
+}
+
+export default async function handler(req, res) {
+
+  try {
+    const completion = await getCompletion(req.query.question)
+    // console.log(completion)
+    res.status(200).json({ completion })
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ error: e })
+  }
 }
