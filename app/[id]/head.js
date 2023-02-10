8470af0df7b6972fec2242ed8b0624233bcb2b35
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-11 02:46:26 +0800

    fix

--- app/[id]/head.js
+++ app/[id]/head.js
@@ -4,6 +4,8 @@ export default async function Head({ params }) {
     const isProd = process.env.NEXT_PUBLIC_NODE_ENV === 'production'
     const baseURL = isProd ? 'https://askme-app-mu.vercel.app' : 'http://localhost:3000'
 
+    console.log(process.env.NEXT_PUBLIC_NODE_ENV)
+    console.log(baseURL)
     const answer = await fetch(`${baseURL}/api/hello?question=${params.id}`, {
         cache: 'no-cache',
         method: 'POST',
