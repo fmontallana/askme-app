3acb164a73f4df4b4a350e9763dbfa6618811ac4
fernando <f.montallana.dit.cvsu@gmail.com>
2023-02-11 10:54:33 +0800

    fix

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-import { getCompletion } from './openai'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
 
   return (
-    <main className={styles.main}>
-
+    <main>
+      Walang laman to. XD
     </main>
   )
 }
