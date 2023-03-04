6a80b4e3ceda455251e65907dfda57e1ac464060
fmontallana <78574637+fmontallana@users.noreply.github.com>
2023-03-04 12:37:01 +0800

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,6 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-  organization: process.env.OPENAI_ORG,
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
